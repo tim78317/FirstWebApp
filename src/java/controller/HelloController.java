ab58fdbb9ff3a52821bd50c466302f21ab782d8d
tliebl <tliebl@L11110042792.wctc.msft>
2016-01-25 18:52:44 -0600

    Switch to mvc branch

--- src/java/controller/HelloController.java
+++ src/java/controller/HelloController.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HelloService;
 
 /**
  *
@@ -35,16 +36,21 @@ public class HelloController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
            String name = request.getParameter("username");
-           String responseMsg = "Hello " + name + " , isn't Java great!";
-           request.setAttribute("myMsg", responseMsg);
            
-           RequestDispatcher view = 
+           HelloService helloSrv = new HelloService();
+           String responseMsg = helloSrv.sayHello(name);
+           
+           request.setAttribute("myMsg", responseMsg);
+            RequestDispatcher view = 
                    request.getRequestDispatcher("/helloResponse.jsp");
            view.forward(request, response);
+          
         } catch(Exception e){
             request.setAttribute("errorMsg", e.getMessage());
         }
+         
     }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -58,18 +64,8 @@ public class HelloController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-          try (PrintWriter out = response.getWriter()) {
-           String age = request.getParameter("age");
-           String responseMsg = "You are " + age + " years old";
-           request.setAttribute("myMsg", responseMsg);
-           
-           RequestDispatcher view = 
-                   request.getRequestDispatcher("/helloResponse.jsp");
-           view.forward(request, response);
-        } catch(Exception e){
-            request.setAttribute("errorMsg", e.getMessage());
-        }
+         processRequest(request, response);
+         
         
     }
 
@@ -85,16 +81,7 @@ public class HelloController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-          try (PrintWriter out = response.getWriter()) {
-           String name = request.getParameter("username");
-           String responseMsg = "Hello " + name + " , isn't Java great!";
-           request.setAttribute("myMsg", responseMsg);
-          } catch(Exception e){
-            request.setAttribute("errorMsg", e.getMessage());
-        }
-             RequestDispatcher view = 
-                   request.getRequestDispatcher("/helloResponse.jsp");
-           view.forward(request, response);
+       
     }
 
     /**
---
+++ src/java/model/HelloService.java
@@ -0,0 +1,13 @@
+
+package model;
+
+/**
+ *
+ * @author tliebl
+ */
+public class HelloService {
+    
+    public String sayHello(String name){
+        return "Hello " + name + " isn't MVC great!";
+    }
+}
